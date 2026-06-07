f40cd88712ff97f863f1187acf3662a4564ddfc1
shubhamjha16 <shubhamjha330@gmail.com>
2026-06-07 20:55:24 +0530

    docs: remove legacy nextauth route directory to fix dynamic route slug conflict

---
+++ client/android/app/src/main/java/com/xoxo/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.xoxo.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- client/app/api/auth/[...nextauth]/route.ts
+++
@@ -1,3 +0,0 @@
-// Legacy NextAuth route file (deprecated in favor of better-auth)
-export const GET = () => new Response("Deprecated", { status: 404 });
-export const POST = () => new Response("Deprecated", { status: 404 });
